9eef88fe2584e9fecddf20c510bc8bae04c4a2a8
Mandeep Singh <mandeepsinghsaggu2@gmail.com>
2022-01-02 20:58:29 +0530

    Shows  Particular Searched or Selected character

--- app/src/main/java/com/oyelabs/marvel/universe/Character_Fragment.kt
+++ app/src/main/java/com/oyelabs/marvel/universe/Character_Fragment.kt
@@ -1,60 +1,51 @@
 package com.oyelabs.marvel.universe
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.marveluniverse.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Character_Fragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Character_Fragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+ lateinit var receiver: BroadcastReceiver
+ var charactersListsFragment:Characters_Fragment?=null
+    lateinit var receiver2:BroadcastReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        charactersListsFragment= Characters_Fragment()
+
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_character_, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Character_Fragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Character_Fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Log.d("SIIIZIE","fragmentCharacter")
+
+        /**Receiving textQuery from MainActivity SearchView*/
+        receiver=object:BroadcastReceiver(){
+            override fun onReceive(context: Context?, intent: Intent?) {
+
+                if(intent?.action.equals("SEND_QUERY_2")) {
+                    var newText = intent?.getStringExtra("newText2").toString()
+                    Log.d("sdsdsds", newText)
                 }
             }
-    }
+        }
+        LocalBroadcastManager.getInstance(requireContext()).registerReceiver(receiver, IntentFilter("SEND_QUERY_2"))
+
+        }
 }
