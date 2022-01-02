0ca4e2f7a0b6e143e7999e5750b1c6e419d1168f
Mandeep Singh <mandeepsinghsaggu2@gmail.com>
2022-01-02 20:52:02 +0530

    created data class for Character Variables

--- app/src/main/java/com/oyelabs/marvel/universe/List_Items.kt
+++ app/src/main/java/com/oyelabs/marvel/universe/List_Items.kt
@@ -1,12 +1,24 @@
 package com.oyelabs.marvel.universe
 
+import android.os.Parcel
+import android.os.Parcelable
 import java.io.Serializable
 
 data class list_items(val id:Int,val name:String,val description:String,
                       val thumbnailArry:Array<String>
-                      ,val series:ArrayList<String>?,val url:String?,):Serializable
+                      ,val series:ArrayList<String>?,val url:String?,):Serializable,Parcelable
 {
-    constructor(id2:Int,name2:String,description2:String,thumbnailArry2:Array<String>):this(id2,name2,description2,thumbnailArry2,null,null)
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.createStringArray()!!,
+        TODO("series"),
+        parcel.readString()
+    ) {
+    }
+
+    constructor(id2:Int, name2:String, description2:String, thumbnailArry2:Array<String>):this(id2,name2,description2,thumbnailArry2,null,null)
 
     override fun equals(other: Any?): Boolean {
         return super.equals(other)
@@ -18,5 +30,27 @@ data class list_items(val id:Int,val name:String,val description:String,
 
     override fun toString(): String {
         return super.toString()
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+        parcel.writeString(name)
+        parcel.writeString(description)
+        parcel.writeStringArray(thumbnailArry)
+        parcel.writeString(url)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<list_items> {
+        override fun createFromParcel(parcel: Parcel): list_items {
+            return list_items(parcel)
+        }
+
+        override fun newArray(size: Int): Array<list_items?> {
+            return arrayOfNulls(size)
+        }
     }
 }
