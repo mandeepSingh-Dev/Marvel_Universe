28eded73c1b21e45f0906294686666b4c942ec94
Mandeep Singh <mandeepsinghsaggu2@gmail.com>
2022-01-02 20:57:15 +0530

    used for search characters in both Fragment Screen by filtering characterslist characters

--- app/src/main/java/com/oyelabs/marvel/universe/MainActivity.kt
+++ app/src/main/java/com/oyelabs/marvel/universe/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.oyelabs.marvel.universe
 
 import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.SearchView
 import android.widget.Toast
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.navigation.fragment.NavHostFragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
@@ -13,43 +23,120 @@ import com.android.volley.VolleyError
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.example.marveluniverse.R
+
 import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("RestrictedApi")
+
+    var myCallback:Characters_Fragment.MyCallback2?=null
+    private  lateinit var searchView:androidx.appcompat.widget.SearchView
+    private lateinit var recyclerViewLinearLayout:LinearLayout
+    private lateinit var recyclerView: RecyclerView
+    private var localBroadcastManager:LocalBroadcastManager?=null
+    private lateinit var receiver2:BroadcastReceiver
+    private var chracterList:ArrayList<list_items>?=null
+    private  var filterListt:ArrayList<String>?=null
+
+
+    @SuppressLint("RestrictedApi", "ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+     super.onCreate(savedInstanceState)
+     setContentView(R.layout.activity_main)
+     searchView = findViewById(R.id.searchview)
+        recyclerView=findViewById(R.id.SearchrecylerView)
+        recyclerViewLinearLayout=findViewById(R.id.lll)
+        chracterList=ArrayList()
+        filterListt=ArrayList()
+        localBroadcastManager = LocalBroadcastManager.getInstance(applicationContext)
 
-        var navHostFragment=supportFragmentManager?.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
-        var navController=navHostFragment.navController
-        var string=navController.currentDestination?.displayName!!
-        Toast.makeText(this,string+"",Toast.LENGTH_SHORT).show()
+     var navHostFragment = supportFragmentManager?.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+     var navController = navHostFragment.navController
+     var string = navController.currentDestination?.displayName!!
+     Log.d("ejreuyruehfn", navController.currentDestination?.label.toString())
+
+     Toast.makeText(this, string + "", Toast.LENGTH_SHORT).show()
 
 
-        /*var requestQueue: RequestQueue?=null
-        requestQueue= Volley.newRequestQueue(this)
-        var publicKey="466d97f4e5bf657ce8780e7b317c57d7"
-        var charactersURL="https://gateway.marvel.com/v1/public/characters?ts=1&apikey="+publicKey+"&hash=4a866f2f4a437b10fcd9744b8286b4db"
-
-            Log.d("dfdfd","dfdf")
-            var jsonObjectRequest: JsonObjectRequest = JsonObjectRequest(Request.Method.GET, charactersURL, null, object : Response.Listener<JSONObject> {
-                override fun onResponse(response: JSONObject?) {
-                    Log.d("dfhujjhjhj","dfdf"+response.toString())
-
-                    val resultsArray = response?.getJSONObject("data")?.getJSONArray("results")
-                    for (i in 0..resultsArray?.length()?.minus(1)!!) {
-                        var resultobject = resultsArray.getJSONObject(i)
-                        var name = resultobject.getString("name")
-                        Log.d("voolyeey", name + "dfdj")
-                    }
+        /**RECEIVING list from CharactersList_Fragment*/
+        receiver2=object: BroadcastReceiver(){
+            override fun onReceive(context: Context?, intent: Intent?) {
+                if(intent?.action.equals("SENDING_LIST")){
+                    Log.d("dfddddddddddd","fsdfds")
+                    chracterList=intent?.getParcelableArrayListExtra<list_items>("charcaterList")
+                    Log.d("fhduhfd","${chracterList?.size}ll")
                 }
-            },
-                object : Response.ErrorListener {
-                    override fun onErrorResponse(error: VolleyError?) {
-                        Log.d("dfhdj",error?.message+"d")
-                    }
-                })
-            requestQueue.add(jsonObjectRequest)*/
-     }
+            }
+        }
+        localBroadcastManager?.registerReceiver(receiver2, IntentFilter("SENDING_LIST"))
+
+
+
+     searchView.setOnQueryTextListener(object :
+         androidx.appcompat.widget.SearchView.OnQueryTextListener {
+         override fun onQueryTextChange(newText: String?): Boolean {
+             var fragmentLabel = navController.currentDestination?.label.toString()
+             if (fragmentLabel.equals("fragment_characterslist_")) {
+                 Log.d("dfdfdfddf", "dfd" + newText)
+
+                 localBroadcastManager?.sendBroadcast(
+                     Intent("SEND_QUERY").putExtra(
+                         "newText",
+                         newText
+                     )
+                 )
+             } else {
+                 filterList(chracterList!!,newText!!)
+                 Log.d("hfjcbcxbcuys",   newText)
+                 localBroadcastManager?.sendBroadcast(Intent("SEND_QUERY_2").putExtra("newText2", newText))
+
+           
+             }
+             return true
+         }
+
+         override fun onQueryTextSubmit(query: String?): Boolean {
+             navController.navigate(R.id.fragment_character_)
+             return true
+         }
+     })
+
+
+
+
+    }
+
+    private fun filterList(arrayList:ArrayList<list_items>, newText:String){
+
+        Log.d("dsdsds",arrayList.size.toString())
+        filterListt?.removeAll(filterListt!!)
+
+         if(newText.equals(" "))
+        {
+
+            Log.d("siizee_SPACE_ELSE",filterListt?.size.toString())
+            recyclerViewLinearLayout.visibility=View.GONE
+            recyclerView.visibility=View.GONE
+        }
+        else if(newText.isEmpty()){
+
+             Log.d("siizee_EMPTY_ELSE",filterListt?.size.toString())
+             recyclerViewLinearLayout.visibility=View.GONE
+             recyclerView.visibility=View.GONE
+
+        }
+        else {
+             recyclerViewLinearLayout.visibility=View.VISIBLE
+             recyclerView.visibility = View.VISIBLE
+             arrayList?.forEach {
+                 if (it.name.lowercase().contains(newText.lowercase())) {
+                     filterListt?.add(it.name)
+                 }
+             }
+             Log.d("siizee ELSE_CON",filterListt?.size.toString())
+             var adapter=MyAdapter_for_SearchList(applicationContext,filterListt!!)
+             recyclerView.layoutManager=LinearLayoutManager(this)
+             recyclerView.adapter=adapter
+         }
+
+    }
 }
